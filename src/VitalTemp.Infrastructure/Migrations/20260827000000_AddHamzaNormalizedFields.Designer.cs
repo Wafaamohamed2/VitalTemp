cb8403cf6cd474abb2851b2c5afeea01fc0cbc3c
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2026-08-28 22:28:21 +0300

    fix: align EF Core migration metadata with runtime version

--- src/VitalTemp.Infrastructure/Migrations/20260827000000_AddHamzaNormalizedFields.Designer.cs
+++ src/VitalTemp.Infrastructure/Migrations/20260827000000_AddHamzaNormalizedFields.Designer.cs
@@ -17,7 +17,7 @@ namespace VitalTemp.Infrastructure.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "10.0.11");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
 
             modelBuilder.Entity("VitalTemp.Domain.Entities.AnalysisResult", b =>
                 {
--- src/VitalTemp.Infrastructure/Migrations/VitalTempDbContextModelSnapshot.cs
+++ src/VitalTemp.Infrastructure/Migrations/VitalTempDbContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace VitalTemp.Infrastructure.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "10.0.11");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
 
             modelBuilder.Entity("VitalTemp.Domain.Entities.AnalysisResult", b =>
                 {
